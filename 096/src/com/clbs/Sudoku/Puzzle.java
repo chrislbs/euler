c8e4c9fe9e69f4383cc0b17e811569b9af043f73
clbs <chrislbs@gmail.com>
2013-08-17 14:05:24 -0500

    Adding initial idea for a puzzle class to be able to work from laptop.

---
+++ 096/src/com/clbs/Sudoku/Puzzle.java
@@ -0,0 +1,14 @@
+package com.clbs.Sudoku;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * User: clbs
+ * Date: 8/17/13
+ * Time: 1:18 PM
+ */
+public class Puzzle {
+    private ArrayList<ArrayList<Integer>> values =
+            new ArrayList<ArrayList<Integer>>();
+}
